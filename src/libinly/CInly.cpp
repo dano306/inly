9a85803d7a9ec9cb0a7831f9a8c4c8bed0977bfb
dano306 <dano306@qq.com>
2014-08-19 11:31:09 +0800

    修正编译规则，待测试

--- src/libinly/CInly.cpp
+++ src/libinly/CInly.cpp
@@ -11,14 +11,17 @@ CInly::~CInly()
 bool
 CInly::CheckLicense(const std_string &r_strLicense)
 {
+	return (false);
 }
 
 CInly::std_string
 CInly::GetLicense(void)
 {
+	return ("");
 }
 
 CInly::std_string
 CInly::DumpLicenseDetail(const std_string &r_strLicense)
 {
+	return ("");
 }
