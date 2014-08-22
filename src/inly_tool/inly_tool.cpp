dc14d8fcba87285a301f04d9cd031c284263cf86
dano306 <dano306@qq.com>
2014-08-22 20:50:49 +0800

    inly_tool增加对GetHardwareString的调用

--- src/inly_tool/inly_tool.cpp
+++ src/inly_tool/inly_tool.cpp
@@ -1,12 +1,17 @@
 #include "CInly.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
 	CInly	Inly;
 	
-	Inly.test("dano306");
+	//Inly.test("dano306");
+	//std::cout << "hello world, dano306!" << std::endl;
 	
-	std::cout << "hello world, dano306!" << std::endl;
+	std::string	strHardware;
+	strHardware = Inly.GetHardwareString();
+	std::cout << "strHardware: " << strHardware << std::endl;
+
 	return (0);
 }
